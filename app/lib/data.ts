0740a0b2f1d244c29614240aeba18f27751b0f33
Jordan Seckler <98609886+jojotoutsec@users.noreply.github.com>
2024-02-27 21:34:23 +0100

    fix error handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore()
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -36,7 +36,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore()
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
         SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -57,7 +57,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore()
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -102,7 +102,7 @@ export async function fetchFilteredInvoices(
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
-  noStore()
+  noStore();
   try {
     const invoices = await sql<InvoicesTable>`
         SELECT invoices.id,
@@ -131,7 +131,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore()
+  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
                             FROM invoices
@@ -152,7 +152,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore()
+  noStore();
   try {
     const data = await sql<InvoiceForm>`
         SELECT invoices.id,
@@ -172,12 +172,11 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
   }
 }
 
 export async function fetchCustomers() {
-  noStore()
+  noStore();
   try {
     const data = await sql<CustomerField>`
         SELECT id,
@@ -195,7 +194,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore()
+  noStore();
   try {
     const data = await sql<CustomersTableType>`
         SELECT customers.id,
@@ -227,7 +226,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore()
+  noStore();
   try {
     const user = await sql`SELECT *
                            FROM users
